888006e650fba68bbe66d8795aa6463ed0c882ce
nilaya1234 <nilaya@qxf2.com>
2020-01-17 12:40:25 +0530

    added all files

---
+++ conf/aws_conf.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'SjdnUends821Jsdlkvxh391ksdODnejdDw'
+
+# Amazon Web Services credentials
+AWS_ACCESS_KEY_ID = 'AKIAJTAYZP5CWD5F3H4A'
+AWS_SECRET_ACCESS_KEY = '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'
+
+
+AWS_REGION = 'ap-south-1'
+BODY_TEXT = 'You have a mail!'
+CHARSET = 'UTF-8'
+SENDER = 'Nilaya <nilaya.indurkar@gmail.com>' # ENTER YOUR NAME and EMAIL
+RECIPIENT = 'nilaya@qxf2.com'          # ENTER YOUR EMAIL
