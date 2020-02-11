b5c46c305a155e275f9ba1435aba2c4f199cedbf
nilaya123 <nilaya@qxf2.com>
2020-02-11 13:54:51 +0530

    Update aws_conf.py

--- conf/aws_conf.py
+++ conf/aws_conf.py
@@ -1,9 +1,9 @@
+#Flask Secret Key
 SECRET_KEY = 'xxxxxxxxxxxxx'
 
 # Amazon Web Services credentials
 AWS_ACCESS_KEY_ID = 'xxxxxxxxxxxxxxxxxxx'
 AWS_SECRET_ACCESS_KEY = 'xxxxxxxxxxxxxxxxx'
-
 
 AWS_REGION = 'xxxxxxxx'
 BODY_TEXT = 'You have a new mail!'
