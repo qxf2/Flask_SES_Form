cced54fff2e045da8d8d49c7c8e923dbd83a687e
nilaya123 <nilaya@qxf2.com>
2020-02-18 10:36:49 +0530

    Update aws_conf.py

--- conf/aws_conf.py
+++ conf/aws_conf.py
@@ -1,11 +1,7 @@
 #Flask Secret Key
 SECRET_KEY = 'xxxxxxxxxxxxx'
 
-# Amazon Web Services credentials
-AWS_ACCESS_KEY_ID = 'xxxxxxxxxxxxxxxxxxx'
-AWS_SECRET_ACCESS_KEY = 'xxxxxxxxxxxxxxxxx'
-
-AWS_REGION = 'xxxxxxxx'
+# Mail Body and Sender mail id
 BODY_TEXT = 'You have a new mail!'
 CHARSET = 'UTF-8'
 SENDER = 'Name <name.surname@gmail.com>' # ENTER YOUR NAME and EMAIL
