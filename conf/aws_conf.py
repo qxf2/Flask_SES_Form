9cda533f1a9c847840d3374a7331ee51b4491cfb
nilaya123 <nilaya@qxf2.com>
2020-02-24 13:51:14 +0530

    Update aws_conf.py

--- conf/aws_conf.py
+++ conf/aws_conf.py
@@ -4,4 +4,4 @@ SECRET_KEY = 'xxxxxxxxxxxxx'
 # Mail Body and Sender mail id
 BODY_TEXT = 'You have a new mail!'
 CHARSET = 'UTF-8'
-SENDER = 'Name <name.surname@gmail.com>' # ENTER YOUR NAME and EMAIL
+SENDER = 'Name <email_id@domain.com>' # ENTER YOUR EMAIL ID abd DOMAIN
